e27616693d09701bcfcdfec586a2aec3780d7f5f
raulavilesrodriguez <finbravilesr@gmail.com>
2024-03-25 22:24:43 -0500

    add files dimens.xml and Type.kt

--- app/src/main/java/com/example/thirtydays/ui/theme/Type.kt
+++ app/src/main/java/com/example/thirtydays/ui/theme/Type.kt
@@ -2,33 +2,43 @@ package com.example.thirtydays.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.thirtydays.R
+
+val Cabin = FontFamily(
+    Font(R.font.cabin_regular, FontWeight.Thin),
+    Font(R.font.cabin_bold, FontWeight.Bold)
+)
+
+val Sriracha = FontFamily(
+    Font(R.font.sriracha_regular, FontWeight.Normal)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Sriracha,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    displayLarge = TextStyle(
+        fontFamily = Sriracha,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = Sriracha,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    displaySmall = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
     )
-    */
 )
