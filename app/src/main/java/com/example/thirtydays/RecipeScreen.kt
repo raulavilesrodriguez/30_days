f47956a301a50559c1431fa8a90408789f60b594
raulavilesrodriguez <finbravilesr@gmail.com>
2024-03-27 12:28:05 -0500

    modified RecipeScreen.kt

--- app/src/main/java/com/example/thirtydays/RecipeScreen.kt
+++ app/src/main/java/com/example/thirtydays/RecipeScreen.kt
@@ -1,21 +1,32 @@
 package com.example.thirtydays
 
+import android.content.res.Configuration
+import android.widget.ImageButton
 import androidx.annotation.StringRes
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -28,53 +39,85 @@ import androidx.compose.ui.unit.dp
 import com.example.thirtydays.model.Recipe
 import com.example.thirtydays.ui.theme.ThirtyDaysTheme
 
+
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun RecipeList(
+    recipes: List<Recipe>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+){
+
+}
+
 @Composable
 fun RecipeItem(
     index: Int,
     recipe: Recipe,
     modifier: Modifier = Modifier
 ){
+    var expanded by remember { mutableStateOf(false) }
+
     Card(
         elevation = CardDefaults.cardElevation(
             defaultElevation = dimensionResource(id = R.dimen.elevation_small)),
         modifier = modifier
     ) {
         Column(
+            horizontalAlignment = Alignment.Start,
             modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
                 .padding(dimensionResource(id = R.dimen.padding_medium))
         ) {
             Text(
-                text = stringResource(id = R.string.title_day, index.toString()),
+                text = stringResource(id = R.string.title_day, index),
                 style = MaterialTheme.typography.displayMedium,
                 color = MaterialTheme.colorScheme.outline
             )
             Text(
-                text = stringResource(id = recipe.descriptionRecipe),
+                text = stringResource(id = recipe.nameRecipe),
                 style = MaterialTheme.typography.displaySmall
             )
             Spacer(Modifier.height(dimensionResource(id = R.dimen.padding_small)))
-            Box(
+            RecipeImageButton(
+                onClick = { expanded = !expanded },
+                imageRecipe = recipe.imageRecipe,
+                descriptionRecipe = recipe.descriptionRecipe,
                 modifier = Modifier
-                    .size(dimensionResource(id = R.dimen.image_size))
-                    .clip(MaterialTheme.shapes.small)
-
-            ){
-                Image(
-                    painter = painterResource(id = recipe.imageRecipe),
-                    contentDescription = stringResource(id = recipe.descriptionRecipe),
-                    alignment = Alignment.TopCenter,
-                    contentScale = ContentScale.FillHeight
-                )
+            )
+            if (expanded) {
+                DescriptionLarge(descriptionRecipe = recipe.descriptionRecipe)
             }
-            DescriptionLarge(descriptionRecipe = recipe.descriptionRecipe)
         }
     }
 }
 
 @Composable
-fun DescriptionLarge(
+private fun RecipeImageButton(
+    onClick: () -> Unit,
+    imageRecipe: Int,
+    descriptionRecipe: Int,
+    modifier: Modifier = Modifier
+){
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(MaterialTheme.shapes.small)
+            .clickable(onClick = onClick)
+    ) {
+        Image(
+            painter = painterResource(id = imageRecipe),
+            contentDescription = stringResource(id = descriptionRecipe),
+            alignment = Alignment.Center,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+    }
+}
+
+
+@Composable
+private fun DescriptionLarge(
     @StringRes descriptionRecipe: Int,
     modifier: Modifier = Modifier
 ){
@@ -85,4 +128,19 @@ fun DescriptionLarge(
     )
 
 }
+
+@Preview("Light Theme")
+@Preview("Dark Theme", uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun RecipePreview(){
+    val recipe = Recipe(
+        R.string.recipe1,
+        R.string.description1,
+        R.drawable.spaghetti1
+    )
+    ThirtyDaysTheme{
+        RecipeItem(index = 1, recipe = recipe)
+    }
+}
+
 
